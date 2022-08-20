3ea150c228424628078f631af174d80a63b12441
Duane Northcutt <duane.northcutt@gmail.com>
2022-08-19 18:41:01 -0700

    Switched to not use gpsd.

--- GPS.py
+++ GPS.py
@@ -13,33 +13,49 @@ import logging
 import time
 
 from geopy import Point
-from gps import gps, WATCH_ENABLE, WATCH_NEWSTYLE
+from pynmeagps import NMEAReader
+from serial import Serial
 
+        (raw, parsed) = nmr.read()
+
+DEF_SERIAL_PORT = "/dev/ttyAMA0"
+STREAM_BAUD_RATE = 9600
+STREAM_TIMEOUT = 3
+
+WARN_INTERVAL = 1000
 
 class GPS():
-    def __init__(self, fixedLocation=None):
-        self.gpsd = gps(mode=(WATCH_ENABLE | WATCH_NEWSTYLE))
-        if (fixedLocation and not isinstance(fixedLocation, Point)):
-            logging.error("The arg 'fixedLocation' must by of type 'Point'")
-            raise RuntimeError("Invalid GPS arg type")
+    def __init__(self, serialPort=DEF_SERIAL_PORT, fixedLocation=None):
+        self.serialPort = serialPort
         self.fixedLocation = fixedLocation
+        if fixedLocation:
+            if not isinstance(fixedLocation, Point):
+                logging.error("The arg 'fixedLocation' must by of type 'Point'")
+                raise RuntimeError("Invalid GPS arg type")
+        else:
+            self.stream = Serial(serialPort, baudrate=STREAM_BAUD_RATE, timeout=STREAM_TIMEOUT)
+            self.nmr = NMEAReader(self.stream)
 
     def getTimeLocation(self, maxWaitTime=None):
         """#### TODO
         """
         if self.fixedLocation:
             return self.fixedLocation
-        utcTime = None
-        lat = None
-        lon = None
-        start = time.time()
+        utcTime, lat, lon = None, None, None
+        lastWarning = start = time.time()
         while None in (lat, lon, utcTime):
-            report = self.gpsd.next()
-            if report['class'] == "TPV":
-                lat = getattr(report, 'lat', None)
-                lon = getattr(report, 'lon', None)
-                utcTime = getattr(report, 'time', None)
-            if maxWaitTime and ((time.time() - start) > maxWaitTime):
+            _, parsedMsg = self.nmr.read()
+            now = time.time()
+            if parsedMsg.msgID == "GGA":
+                if parsedMsg.quality == 0:
+                    if ((now - lastWarning) > WARN_INTERVAL):
+                        logging.warning("GPS quality issue: invalid data")
+                        lastWarning = now
+                    continue
+                lat = parsedMsg.lat
+                lon = parsedMsg.lon
+                utcTime = parsedMsg.time  #### FIXME make the be the right format
+            if maxWaitTime and ((now - start) > maxWaitTime):
                 logging.warning("Returned without GPS information")
                 return None, None
             time.sleep(.1)
