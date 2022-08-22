dad6de5ee5d473bfdfcbafea0cdea7dc3369b7da
Duane Northcutt <duane.northcutt@gmail.com>
2022-08-21 20:12:02 -0700

    Refactored.

--- pocket1090.sh
+++ pocket1090.sh
@@ -13,7 +13,7 @@
 COMMANDS="install, start, stop, status, clean"
 
 if [ "$#" -ne 1 ]; then
-    echo "Must provide command, i.e., one of: ${COMMANDS}"
+    echo "ERROR: Must provide command, i.e., one of: ${COMMANDS}"
     exit 1
 fi
 
@@ -47,41 +47,48 @@ fi
 
 export PYTHONPATH="${PYTHONPATH}:${INSTALL_PATH}"
 
-if [ "${CMD}" == "install" ]; then
+install() {
     if [ -d "${INSTALL_PATH}" ]; then
-        echo "Install directory exists"
+        echo "    Install directory exists"
     else
-        echo "Creating install directory: ${INSTALL_PATH}"
+        echo "    Creating install directory: ${INSTALL_PATH}"
         sudo mkdir ${INSTALL_PATH}
         sudo chmod 0755 ${INSTALL_PATH}
     fi
     ASSETS_PATH="${INSTALL_PATH}assets"
     if [ -d "${ASSETS_PATH}" ]; then
-        echo "Assets directory exists"
+        echo "    Assets directory exists"
     else
-        echo "Creating assets directory: ${ASSETS_PATH}"
+        echo "    Creating assets directory: ${ASSETS_PATH}"
         sudo mkdir "${ASSETS_PATH}"
     fi
 
-    echo "Copying files to ${INSTALL_PATH}"
+    echo "    Copying files to ${INSTALL_PATH}"
     sudo cp ${DUMP1090_BIN} ${INSTALL_PATH}
     sudo cp ${POCKET1090_PATH}*.py ${INSTALL_PATH}
 
-    echo "Asset files to ${ASSETS_PATH}"
+    echo "    Asset files to ${ASSETS_PATH}"
     sudo cp ${POCKET1090_PATH}assets/*.png "${ASSETS_PATH}"
-elif [ "${CMD}" == "start" ]; then
-    echo "Starting the dump1090 server in the background"
-#    ${INSTALL_PATH}/dump1090 --write-json ${JSON_FILE_PATH} > ${DUMP1090_OUTPUT_FILE} &
+}
 
-    echo "Starting the pocket1090 application in the background"
-#    ${INSTALL_PATH}/pocket1090.py ${VERBOSE} ${OPTIONS} ${JSON_FILE_PATH} &
-elif [ "${CMD}" == "stop" ]; then
-    echo "Stopping the pocket1090 application"
+start() {
+    echo "    Starting the dump1090 server in the background"
+    ${INSTALL_PATH}dump1090 --quiet --metric --write-json ${JSON_FILE_PATH} &
+
+    echo "     Starting the pocket1090 application in the background"
+    echo ${INSTALL_PATH}pocket1090.py ${VERBOSE} ${OPTIONS} ${JSON_FILE_PATH} &
+}
+
+stop() {
     for KILLPID in `ps -ef | awk '$9~/^.*pocket1090.py$/ {print $2}'`; do kill $KILLPID; done
 
-    echo "Stopping the dump1090 server"
+    echo "    Stopping the dump1090 server"
     for KILLPID in `ps -ef | awk '$8~/^.*dump1090$/ {print $2}'`; do kill $KILLPID; done
-elif [ "${CMD}" == "status" ]; then
+
+    status
+}
+
+status() {
     DUMP_PIDS=`ps -ef | awk '$8~/^.*dump1090$/ {print $2}'`
     if [ $? == 0 ] && [ "$DUMP_PIDS" != "" ]; then
         echo "    dump1090 Server Running:" $DUMP_PIDS
@@ -94,8 +101,27 @@ elif [ "${CMD}" == "status" ]; then
     else
         echo "    pocket1090 Application Not Running"
     fi
-elif [ "${CMD}" == "clean" ]; then
+}
+
+clean() {
     rm -rf "${INSTALL_PATH}"
+}
+
+if [ "${CMD}" == "install" ]; then
+    echo "  Installing pocket1090..."
+    install
+elif [ "${CMD}" == "start" ]; then
+    echo "  Starting pocket1090..."
+    start
+elif [ "${CMD}" == "stop" ]; then
+    echo "    Stopping the pocket1090 application"
+    stop
+elif [ "${CMD}" == "status" ]; then
+    echo "  Getting status of pocket1090"
+    status
+elif [ "${CMD}" == "clean" ]; then
+    echo "  Removing the installed pocket1090 files/directories"
+    clean
 else
     echo "ERROR: invalid command: ${CMD} -- must be one of ${COMMANDS}"
     exit 1
